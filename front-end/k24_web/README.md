32cff8069ff2cdb5107a2b2279845b4db2afe4cf
Syed A Bilgrami <bilgrami@gmail.com>
2020-09-22 17:01:53 -0700

    initial commit

---
+++ front-end/k24_web/README.md
@@ -0,0 +1,4 @@
+## karachi24 Web App
+A repo showing important events, news, and community updates related to Karachi and areas around it.
+
+
---
+++ front-end/k24_web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Welcome to Karachi24.com
+      </header>
+    </div>
+  );
+}
+
+export default App;
