0dd7eb38f52496173d40daedf84a032562e4cb94
Syed A Bilgrami <bilgrami@gmail.com>
2020-10-10 16:18:22 -0700

    removed use of .env

--- backend/K24DataHarvester/invoke.aws.sh
+++ backend/K24DataHarvester/invoke.aws.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# pls replace this with your twitter key/secret
+TwitterKey=*** twitterkey ***
+TwitterSecretKey=***twiiter secret key ***
+# pls replace this with your own s3 bucket that you have write access to
+InputBucketName=data.karachi24.com
+region=us-west-2
+
+# below settings don't need to be changed
 Environment=aws
 eventFile_TweetSearcherFunction=./functions/TweetSearcher/event.json
 eventFile_SaveToDynamoDbFunction=./data/result1.json
-TwitterKey=*** twitterkey ***
-TwitterSecretKey=***twiiter secret key ***
 DBTableName=K24Tweets
-InputBucketName=data.karachi24.com
 RootFolderForTweets=k24dataharvester/tweets
-region=us-west-2
 Debug=0
 # ---------- no changes beyond this point ----------
 
 # echo Validating SAM template
-# sam validate -t template.yaml
+sam validate -t template.yaml
 
 # echo Building CloudFormation stack
 sam build
@@ -30,7 +34,7 @@ ParameterKey=RootFolderForTweets,ParameterValue=$RootFolderForTweets \
 ParameterKey=Debug,ParameterValue=$Debug \
 --skip-pull-image \
 --region $region \
---env-vars env.json -e $eventFile_TweetSearcherFunction \
+-e $eventFile_TweetSearcherFunction \
 > $eventFile_SaveToDynamoDbFunction
 
 echo Executing SaveToDynamoDbFunction
@@ -41,7 +45,6 @@ ParameterKey=DBTableName,ParameterValue=$DBTableName \
 ParameterKey=Debug,ParameterValue=$Debug \
 --skip-pull-image \
 --region $region \
---env-vars env.json \
 -e $eventFile_SaveToDynamoDbFunction \
 > ./data/result2.json
 
