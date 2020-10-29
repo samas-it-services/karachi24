e928cc1b8c8af72902effc9f3c2efe79b580f401
Syed A Bilgrami <bilgrami@gmail.com>
2020-10-29 04:40:09 -0700

    updated query

--- backend/k24_web/src/public-client-node.js
+++ backend/k24_web/src/public-client-node.js
@@ -1,30 +1,44 @@
 const axios = require("axios")
 
-const url = process.env.API_URL
-const api_key = process.env.API_<YOUR_API_NAME>_GRAPHQLAPIKEYOUTPUT
+// const url = process.env.API_KARACHI24API_GRAPHQLAPIENDPOINTOUTPUT
+// const api_key = process.env.API_KARACHI24API_GRAPHQLAPIKEYOUTPUT
 
 const gql_query = `
-    query listK24Tweets {
-        listK24Tweetss(limit: 10) {
-        items {
-            id
-            text
-            screen_name
-            row_timestamp
-            retweeted
-            retweet_count
-            reply_count
-            location
-            last_updated
-            category
-            enabled
-            followers_count
-            file_path
-            favorite_count
-        }
-        nextToken
-        }
+query listK24Tweets {
+  result: listK24Tweetss(limit: 10) {
+    items {
+      id
+      text
+      screen_name
+      row_timestamp
+      retweeted
+      retweet_count
+      reply_count
+      location
+      last_updated
+      category
+      enabled
+      followers_count
+      file_path
+      favorite_count
+      flagged
+      flaggedData
+      hashtags
+      media_url_https
+      source_device
+      topic
+      truncated
+      tweet_created_at
+      tweet_date
+      user_name
+      user_status_count
+      video_url
+      duration_millis
+      createdAt
     }
+    nextToken
+    }
+}
 `  
 
 axios({
